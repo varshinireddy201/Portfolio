9f9afcdcfcb83da834437a3bf8426882e1f68ec3
varshinireddy201 <47922510+varshinireddy201@users.noreply.github.com>
2024-07-15 02:12:27 +0000

    Deploying to gh-pages from @ varshinireddy201/Portfolio@9c16c582697b112b8a41e941e5e8b58e32c12aa3 🚀

---
+++ _next/static/Y5uLHFj7gSPZBh6o5CqnX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4d6c388929626d1f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
